a6547b8802a124cfb7718c48efccc9aee55557d6
Guillermo <59940188+DARKBOW923@users.noreply.github.com>
2023-02-14 22:47:44 +0100

    Update Apuntarse.php

--- mvc_infobdn_final/views/Apuntarse.php
+++ mvc_infobdn_final/views/Apuntarse.php
@@ -1,9 +1,9 @@
 
 echo <form action="../index.php?controller=MatriculaController&action=insertarEnMatricula" method="post">
-    ISBN
-    <input type="text" name = "ID_ALUMNO">
-    <input type="text" name = "ID_CURSO">
+    
+   ID DE TU CUENTA <input type="text" name = "ID_ALUMNO">
+    ID DEL CURSO <input type="text" name = "ID_CURSO">
     <br/>
-    <input type = "submit" value="Añadir ALYUMNO">
+    <input type = "submit" value="APUNTARSE">
 </form>
 
